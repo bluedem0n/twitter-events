7b29747c2e41c3df21076359d231ddc8548d80c9
Sthefany Floriano <sthefanyflorianog@gmail.com>
2016-09-20 00:14:03 -0500

    Contador funcionando

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 window.onload = function(){
-      var timeline = document.getElementById("timeline"),
+    var timeline = document.getElementById("timeline"),
         tweet = document.getElementById("tweet"),
         btnNuevoTweet = document.getElementById("btn-agregar");
+    
     var agregarTweet = function () {
         var tarea = tweet.value,
             nuevoTweet = document.createElement("li"),
             enlace = document.createElement("a"),
             contenido = document.createTextNode(tarea);
+        
         if(tarea === "") {
             tweet.setAttribute("placeholder", "Agrega un Tweet válido");
             tweet.className = "error";
@@ -24,17 +26,38 @@ window.onload = function(){
         });
     }
     };
+    
+    /* Comprobando Input */
+    
     var comprobarInput = function(){
         tweet.className = "";
         tweet.setAttribute = ("placeholder","Agrega una Tweet");
         
     }; 
+    
+    /* Eliminando Tweet */
+    
     var eliminarTweet = function(){
         this.parentNode.removeChild(child);
     };
+    
+    /* Contando Caracteres */
+    var contarCaracteres = function(){
+        btnNuevoTweet.disabled=false; 
+        var limite = 140;
+        var longitud = document.getElementById("tweet").value.length;
+        document.getElementById("contar").innerHTML = limite - longitud + " caracteres restantes"; 
+        if (longitud >= limite){
+            document.getElementById("contar").classList.add("limite");
+            btnNuevoTweet.disabled = true;
+        }  
+    }
 
+    /* Eventos */
+    
     btnNuevoTweet.addEventListener("click",agregarTweet);
     tweet.addEventListener("click",comprobarInput);
+    tweet.addEventListener("keydown", contarCaracteres);
 
     for (var i = 0; i <= timeline.children.length -1; i++){
         
